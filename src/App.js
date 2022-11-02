4e57dece7025b6638a50f1d6179d64fa3e12e9b3
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-02 21:13:38 +0200

    Add styling to expense-item

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div>
-      <h2>Let's get started Bitch!</h2>
-      <p>This is also visible</p>
+      <h2>Let's get started</h2>
       <ExpenseItem />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
 
     return (
-        <div>
+        <div className="expense-item">
             <h2>March 28th 2022</h2>
-            <div>
+            <div className="expense-item__description">
                 <h2>Car Insurance</h2>
-                <div>$420.69</div>
+                <div className="expense-item__price">$420.69</div>
             </div>
         </div>
     )
