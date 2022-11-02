1b0ef5fe23ca9dc1fafec672d16f824ef340c579
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-02 21:34:04 +0200

    Test the usage of props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+
+  const expenses = [
+    {
+      title: "Toilet Paper",
+      amount: 420.69,
+      date: new Date(2022, 2, 18)
+    }
+  ]
+
   return (
     <div>
       <h2>Let's get started</h2>
-      <ExpenseItem />
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
+function ExpenseItem(props) {
 
     return (
         <div className="expense-item">
-            <h2>March 28th 2022</h2>
+            <h2>{props.date.toDateString()}</h2>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
-                <div className="expense-item__price">$420.69</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     )
