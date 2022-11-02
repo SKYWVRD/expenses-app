1a36100ad10d334048868c9cae23229437183744
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-02 21:07:29 +0200

    Add files from non git folder

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem() {
+
+    return (
+        <div>
+            <h2>March 28th 2022</h2>
+            <div>
+                <h2>Car Insurance</h2>
+                <div>$420.69</div>
+            </div>
+        </div>
+    )
+}
+
+export default ExpenseItem
