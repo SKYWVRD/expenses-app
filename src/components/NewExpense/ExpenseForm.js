7c8be93196fb2785e6b79aa7a4736e1485b36387
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-09 21:10:56 +0200

    Add clearing form functionality

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -30,6 +30,9 @@ const ExpenseForm = () => {
         };
 
         console.log(expenseData)
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
 
     }
 
@@ -38,15 +41,24 @@ const ExpenseForm = () => {
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text' onChange={titleChangeHandler} />
+                    <input type='text' 
+                    value={enteredTitle}
+                    onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01' onChange={amountChangeHandler} />
+                    <input type='number'
+                        min='0.01' 
+                        value={enteredAmount}
+                        onChange={amountChangeHandler} />
                 </div>
                 <div className='new-expense__control' onChange={dateChangeHandler}>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01' />
+                    <input type='date' 
+                    min='2019-01-01'
+                    value={enteredDate}
+                    onChange={dateChangeHandler}
+                    />
                 </div>
             </div>
             <div className='new-expense__actions'>
