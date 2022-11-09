0932f4d541e02905970aec7d70026cbe4cb7b64b
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-09 20:14:53 +0200

    Add title change handler to ExpenseForm

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,21 +3,24 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () => {
 
+    const titleChangeHandler = (event) => {
+        console.log(event.target.value);
+    }
 
     return (
         <form>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type='text'/>
+                    <input type='text' onChange={titleChangeHandler} />
                 </div>
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type='number' min='0.01'/>
+                    <input type='number' min='0.01' />
                 </div>
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type='date' min='2019-01-01'/>
+                    <input type='date' min='2019-01-01' />
                 </div>
             </div>
             <div className='new-expense__actions'>
