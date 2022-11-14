797a781e5130972fef8e3ccf5c8879358c1c26a7
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-14 20:55:58 +0200

    Add inserting new expense logic

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React, { useState } from 'react'
+
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense'
 
+const DUMMY_DATA = [
+  {
+    title: "Toilet Paper",
+    amount: 420.69,
+    date: new Date(2022, 2, 18)
+  },
+  {
+    title: "Car Insurance",
+    amount: 2602.19,
+    date: new Date(2022, 3, 1)
+  }
+]
+
 const App = () => {
 
-  const expenses = [
-    {
-      title: "Toilet Paper",
-      amount: 420.69,
-      date: new Date(2022, 2, 18)
-    },
-    {
-      title: "Car Insurance",
-      amount: 2602.19,
-      date: new Date(2022, 3, 1)
-    }
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_DATA)
 
   const addExpenseHandler = (expense) => {
-    console.log(expense);
+    setExpenses(prevExpenses => {
+      return [expense, ...expenses]
+    })
   }
 
   return (
