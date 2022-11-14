7fa4396c61b9532ad23933a413584fec08f699e9
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-14 20:39:39 +0200

    Add year filter dropdown component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,21 @@
+import React, { useState } from 'react'
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card"
+import ExpenseFilter from './ExpenseFilter'
 
 
 const Expenses = (props) => {
 
+    const [filteredYear, setYear] = useState('2019');
+
+    const filterChoiceHandler = (selectedYear) => {
+        setYear(selectedYear);
+    }
+
     return (
         <Card className='expenses'>
+            <ExpenseFilter selected={filteredYear} filterChoiceHandler={filterChoiceHandler} />
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
