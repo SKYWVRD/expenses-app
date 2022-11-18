d55ba73ec12b3f8d30e674dd5c4567f5383b26c7
Sean Boonzaier <boonzaiersa@gmail.com>
2022-11-18 21:33:04 +0200

    Change ExpenseForm component to use module style css

--- src/App.js
+++ src/App.js
@@ -1,39 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import Expenses from './components/Expenses/Expenses'
-import NewExpense from './components/NewExpense/NewExpense'
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const DUMMY_DATA = [
   {
     id: "e1",
     title: "Toilet Paper",
     amount: 420.69,
-    date: new Date(2022, 2, 18)
+    date: new Date(2022, 2, 18),
   },
   {
     id: "e2",
     title: "Car Insurance",
     amount: 2602.19,
-    date: new Date(2022, 3, 1)
-  }
-]
+    date: new Date(2022, 3, 1),
+  },
+];
 
 const App = () => {
-
   const [expenses, setExpenses] = useState(DUMMY_DATA);
 
   const addExpenseHandler = (expense) => {
-    setExpenses(prevExpenses => {
-      return [expense, ...prevExpenses]
-    })
-  }
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-      <NewExpense onAddExpense = {addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
